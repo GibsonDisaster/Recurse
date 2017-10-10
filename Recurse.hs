f17ef911713d9a49dcfdcd3c6d83e898af6c31f2
Henning Tonko <henninghtonko@gmail.com>
2017-10-10 18:46:35 -0500

    added examples and four corners

--- Recurse.hs
+++ Recurse.hs
@@ -2,55 +2,31 @@ module Recurse where
   import Graphics.Gloss
   import Graphics.Gloss.Data.ViewPort
 
-  data DrawInstruct = SmallToBig | BigToSmall | FourCorners Bool deriving Eq
+  data DrawInstruct = SmallToBig | BigToSmall | FourCorners Int deriving Eq
 
-  recurseCircle :: Float -> Float -> Float -> Color -> DrawInstruct -> [Picture]
-  recurseCircle x y r c di
-    | r < 0.05 = [translate 0 0 (color black (circle r))]
-    | di == BigToSmall = [translate x y (color c (circle r))] ++ recurseCircle (x+45) y (r * 0.9) c di
-    | di == SmallToBig = [translate x y (color c (circle r))] ++ recurseCircle (x-45) y (r * 0.9) c di
-    | di == (FourCorners True) = [translate x y (color c (circle r))] ++ recurseCircle (x-45) (y-45) (r * 0.9) c (FourCorners False) ++ recurseCircle (x-45) (y+45) (r*0.9) c (FourCorners False) ++ recurseCircle (x+45) (y-45) (r*0.9) c (FourCorners False) ++ recurseCircle (x+45) (y+45) (r*0.9) c (FourCorners False)
-    | di == (FourCorners False) = [translate (x-45) (y-45) $ color c $ circle r]
+  recurseCircle :: Float -> Float -> Float -> Float -> Color -> DrawInstruct -> [Picture]
+  recurseCircle spacing x y r c di
+    | r < 0.01 = []
+    | di == BigToSmall = [translate x y (color c (circle r))] ++ recurseCircle spacing (x+spacing) y (r * 0.9) c di
+    | di == SmallToBig = [translate x y (color c (circle r))] ++ recurseCircle spacing (x-spacing) y (r * 0.9) c di
+    | di == (FourCorners 0) = [translate (x) (y) $ color c $ circle r] ++ recurseCircle spacing (x-spacing) (y+spacing) (0.8 * r) c (FourCorners 1) ++ recurseCircle spacing (x+spacing) (y+spacing) (0.8 * r) c (FourCorners 2) ++ recurseCircle spacing (x+spacing) (y-spacing) (0.8 * r) c (FourCorners 3) ++ recurseCircle spacing (x-spacing) (y-spacing) (0.8 * r) c (FourCorners 4)
+    | di == (FourCorners 1) = [translate x y $ color c $ circle r] ++ recurseCircle spacing (x-spacing) (y+spacing) (0.7 * r) c (FourCorners 1)
+    | di == (FourCorners 2) = [translate x y $ color c $ circle r] ++ recurseCircle spacing (x+spacing) (y+spacing) (0.7 * r) c (FourCorners 2)
+    | di == (FourCorners 3) = [translate x y $ color c $ circle r] ++ recurseCircle spacing (x+spacing) (y-spacing) (0.7 * r) c (FourCorners 3)
+    | di == (FourCorners 4) = [translate x y $ color c $ circle r] ++ recurseCircle spacing (x-spacing) (y-spacing) (0.7 * r) c (FourCorners 4)
     | otherwise = []
 
-  recurseRect :: Float -> Float -> Float -> Color -> DrawInstruct -> [Picture]
-  recurseRect x y w c di
-    | w < 0.05 = [translate 0 0 (color black (rectangleWire w w))]
-    | otherwise = [translate x y (color c (rectangleWire (w*2) w))] ++ recurseRect x (if di == SmallToBig then y+45 else y-45) (w * 0.9) c di
+  recurseRect :: Float -> Float -> Float -> Float -> Color -> DrawInstruct -> [Picture]
+  recurseRect spacing x y w c di
+    | w < 0.01 = []
+    | di == BigToSmall = [translate x y (color c (rectangleWire w w))] ++ recurseRect spacing (x+spacing) y (w * 0.9) c di
+    | di == SmallToBig = [translate x y (color c (rectangleWire w w))] ++ recurseRect spacing (x-spacing) y (w * 0.9) c di
+    | di == (FourCorners 0) = [translate (x) (y) $ color c $ rectangleWire w w] ++ recurseRect spacing (x-spacing) (y+spacing) (0.8 * w) c (FourCorners 1) ++ recurseRect spacing (x+spacing) (y+spacing) (0.8 * w) c (FourCorners 2) ++ recurseRect spacing (x+spacing) (y-spacing) (0.8 * w) c (FourCorners 3) ++ recurseRect spacing (x-spacing) (y-spacing) (0.8 * w) c (FourCorners 4)
+    | di == (FourCorners 1) = [translate x y $ color c $ rectangleWire w w] ++ recurseRect spacing (x-spacing) (y+spacing) (0.7 * w) c (FourCorners 1)
+    | di == (FourCorners 2) = [translate x y $ color c $ rectangleWire w w] ++ recurseRect spacing (x+spacing) (y+spacing) (0.7 * w) c (FourCorners 2)
+    | di == (FourCorners 3) = [translate x y $ color c $ rectangleWire w w] ++ recurseRect spacing (x+spacing) (y-spacing) (0.7 * w) c (FourCorners 3)
+    | di == (FourCorners 4) = [translate x y $ color c $ rectangleWire w w] ++ recurseRect spacing (x-spacing) (y-spacing) (0.7 * w) c (FourCorners 4)
+    | otherwise = []
 
   drawRecurse :: Picture -> Color -> IO ()
   drawRecurse p c = display FullScreen black p
-
-  {- import Graphics.Gloss
-  import Graphics.Gloss.Data.ViewPort
-
-  data DrawInstruct = SmallToBig | BigToSmall | FourCorners deriving (Show, Eq)
-
-  data Direction = TopToBottom | BottomToTop | LeftToRight | RightToLeft | Nothing deriving (Eq, Show)
-
-  data Shape = Rect Point Float Float Color | Circ Point Float Float Color deriving (Eq, Show)
-
-  class ShapeClass a where
-    center :: a -> Point
-    top :: a -> Float
-    bottom :: a -> Float
-    left :: a -> Float
-    right :: a -> Float
-    width :: a -> Float
-    height :: a -> Float
-
-  instance ShapeClass Shape where
-    center (Rect c _ _ _) = c
-    top (Rect (_, y) _ h _) = y+h
-    bottom (Rect (_, y) _ h _) = y-h
-    left (Rect (x, _) w _ _) = x-w
-    right (Rect (x, _) w _ _) = x+w
-    width (Rect _ w _ _) = w
-    height (Rect _ _ h _) = h
-
-  createPicture :: DrawInstruct -> Shape -> Direction -> [Picture]
-  createPicture SmallToBig (Rect (x, y) w h c) di = [translate x y $ color c $ rectangleWire w h] ++ (if w > 0.01 then createPicture SmallToBig (Rect ((if di == LeftToRight then x+30 else x-30), y) (w*0.9) (h*0.9) c) di else [])
-  createPicture _ _ _ = [translate 0 0 $ color red $ polygon [(0, 0), (0+0, 0), (0, 0), (0, 0)]]
-
-  drawRecurse :: Picture -> IO ()
-  drawRecurse p = display FullScreen black p -}
